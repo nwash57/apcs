6b41fa64e96d4695af1907d90d7bb9e83a82dd19
nwash57 <nwash57@gmail.com>
2015-03-06 10:38:49 -0600

    blah

--- MazeGenerator/src/MazeTest.java
+++ MazeGenerator/src/MazeTest.java
@@ -1,5 +1,7 @@
 //this is a test
 
+import java.util.Scanner;
+
 public class MazeTest
 {
     public static void main(String[] args)
@@ -12,13 +14,22 @@ public class MazeTest
 	    4: finalToPng
 	     */
 	    boolean[] ms = new boolean[5];
-	    ms[0] = true;
+	    ms[0] = false;
 	    ms[1] = false;
 	    ms[2] = false;
-	    ms[3] = true;
-	    ms[4] = true;
+	    ms[3] = false;
+	    ms[4] = false;
 
-        Maze m = new Maze(50, 50, 5, ms[0], ms[1], ms[3]);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter grid width: ");
+        int width = scan.nextInt();
+        System.out.println("Enter grid height: ");
+        int height = scan.nextInt();
+        System.out.println("Enter maximum path length: ");
+        int pathLength = scan.nextInt();
+        System.out.println();
+
+        Maze m = new Maze(width, height, pathLength, ms[0], ms[1], ms[2]);
         m.generateGrid();
         m.fillGrid();
 	    m.fillExtraPath();
