22c1e337724e4a787437cbc0242fddee2825a3f3
nwash57 <nwash57@gmail.com>
2015-03-06 10:31:28 -0600

    implemented 'loading'

--- MazeGenerator/src/MazeTest.java
+++ MazeGenerator/src/MazeTest.java
@@ -12,13 +12,15 @@ public class MazeTest
 	    4: finalToPng
 	     */
 	    boolean[] ms = new boolean[5];
-	    ms[0] = true;
+	    ms[0] = false;
 	    ms[1] = false;
 	    ms[2] = false;
 	    ms[3] = true;
 	    ms[4] = true;
 
-        Maze m = new Maze(50, 50, 5, ms[0], ms[1], ms[3]);
+	    System.out.println("loading|....................................................................................................|");
+	    System.out.print("       |");
+        Maze m = new Maze(300, 300, 5, ms[0], ms[1], ms[2]);
         m.generateGrid();
         m.fillGrid();
 	    m.fillExtraPath();
